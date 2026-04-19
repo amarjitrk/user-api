2882445b18eb3412c344c39f757ff2f340623b82
Amarjit Dey <amarjit.dey2002@gmail.com>
2026-04-19 19:55:55 +0530

    Initial commit

---
+++ src/main/java/com/example/model/User.java
@@ -0,0 +1,10 @@
+//package com.example.model;
+//
+//import jakarta.validation.constraints.NotBlank;
+//
+//public class User {
+//    private Long id;
+//
+//    @NotBlank
+//    private String Name;
+//}
